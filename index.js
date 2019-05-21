04600c489ae928df1b71226660ba3719e44dfa2a
Vik Vangala <vvang044@gmail.com>
2019-05-21 17:45:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
+
+function happyHolidaysTo(holidays,name){
+  return `Happy ${holiday}, ${name}!`
+}
